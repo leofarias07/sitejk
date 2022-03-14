98c04a0775e9a49fbebfd8c5450d1062dd88f285
leofarias07 <leo.farias.05@gmail.com>
2022-03-14 11:44:22 -0300

    hero 1.8

--- src/pages/obras/index.tsx
+++ src/pages/obras/index.tsx
@@ -1,37 +1,11 @@
 import Header from '../../components/Header';
-import ObrasItens from '../../components/ObraItens';
 import { ObrasContainer } from '../../styles/obrasStyles';
 
 export default function Obras() {
   return (
     <ObrasContainer>
       <Header />
-      <main className="container">
-        <ObrasItens
-          title="Obra 01"
-          city="Caxias-MA"
-          slug="teste"
-          imgUrl="https://images.prismic.io/sitejk/44f1a0fe-345c-4013-a11f-19b8f5f5ec89_transferir.jpg?auto=compress,format"
-        />
-        <ObrasItens
-          title="Obra 01"
-          city="Caxias-MA"
-          slug="teste"
-          imgUrl="https://images.prismic.io/sitejk/44f1a0fe-345c-4013-a11f-19b8f5f5ec89_transferir.jpg?auto=compress,format"
-        />
-        <ObrasItens
-          title="Obra 01"
-          city="Caxias-MA"
-          slug="teste"
-          imgUrl="https://images.prismic.io/sitejk/44f1a0fe-345c-4013-a11f-19b8f5f5ec89_transferir.jpg?auto=compress,format"
-        />
-        <ObrasItens
-          title="Obra 01"
-          city="Caxias-MA"
-          slug="teste"
-          imgUrl="https://images.prismic.io/sitejk/44f1a0fe-345c-4013-a11f-19b8f5f5ec89_transferir.jpg?auto=compress,format"
-        />
-      </main>
+      <main className="container" />
     </ObrasContainer>
   );
 }
